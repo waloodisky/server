d11ad1d38a0d5ffd63586dea3ad2068458bb3f61
Glitch (makimaserver) <none>
2024-08-29 09:10:17 +0000

    🦯🥛 Updated with Glitch

--- server.js
+++ server.js
@@ -1,76 +1,57 @@
-const express = require('express');
-
-const http = require('http');
-
-const socketIO = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors"); // Import CORS
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = socketIO(server);
 
 const waitingPlayers = [];
 
+// Enable CORS for all origins
+app.use(cors());
 
 // Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-io.on('connection', (socket) => {
+  // Notify everyone that a new user has connected
+  io.emit("userConnected", "A new player has connected!");
 
-    console.log('A user connected');
+  waitingPlayers.push(socket);
+  console.log(`Player added to queue. Queue length: ${waitingPlayers.length}`);
 
-    waitingPlayers.push(socket);
+  if (waitingPlayers.length >= 2) {
+    const player1 = waitingPlayers.shift();
+    const player2 = waitingPlayers.shift();
 
-    console.log(`Player added to queue. Queue length: ${waitingPlayers.length}`);
+    const roomId = Math.random().toString(36).substring(2, 9);
+    player1.join(roomId);
+    player2.join(roomId);
 
-    if (waitingPlayers.length >= 2) {
+    player1.emit("roomCreated", roomId);
+    player2.emit("roomCreated", roomId);
 
-        const player1 = waitingPlayers.shift();
+    io.to(roomId).emit("gameStart");
+  }
 
-        const player2 = waitingPlayers.shift();
+  socket.on("click", (roomId) => {
+    io.to(roomId).emit("updateClick", socket.id);
+  });
 
-        const roomId = Math.random().toString(36).substring(2, 9);
-
-        player1.join(roomId);
-
-        player2.join(roomId);
-
-        player1.emit('roomCreated', roomId);
-
-        player2.emit('roomCreated', roomId);
-
-        io.to(roomId).emit('gameStart');
-
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    const index = waitingPlayers.indexOf(socket);
+    if (index !== -1) {
+      waitingPlayers.splice(index, 1);
     }
-
-    socket.on('click', (roomId) => {
-
-        io.to(roomId).emit('updateClick', socket.id);
-
-    });
-
-    socket.on('disconnect', () => {
-
-        console.log('A user disconnected');
-
-        const index = waitingPlayers.indexOf(socket);
-
-        if (index !== -1) {
-
-            waitingPlayers.splice(index, 1);
-
-        }
-
-        io.emit('playerDisconnected', socket.id);
-
-    });
-
+    io.emit("playerDisconnected", socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-
-    console.log(`Server is running on port ${PORT}`);
-
+  console.log(`Server is running on port ${PORT}`);
 });
